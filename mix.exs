6d9689ac6f389e3eb0c6244207c2ae563a120608
Ian Duggan <ian@ianduggan.net>
2017-11-07 01:50:29 -0800

    Adjust rebar compile

--- mix.exs
+++ mix.exs
@@ -54,7 +54,7 @@ defmodule Taggart.Mixfile do
       {:phoenix, "~> 1.3.0", only: [:dev, :test], runtime: false},
       {:benchee, "~> 0.9.0", only: [:dev, :test], runtime: false},
       {:exprof, "~> 0.2.1", only: [:dev, :test], runtime: false},
-      {:eflame, github: "ijcd/eflame", compile: "rebar compile"},
+      {:eflame, github: "ijcd/eflame", compile: "rebar compile", only: [:dev, :test], runtime: false},
       
       {:mix_test_watch, "~> 0.3", only: [:dev, :test], runtime: false},
       {:credo, "~> 0.8.5", only: [:dev, :test], runtime: false},
